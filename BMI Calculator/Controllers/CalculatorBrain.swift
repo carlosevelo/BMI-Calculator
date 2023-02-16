f46e749df908f0bd7c01b980fcb76aabfba5bb58
Carlos Evelo <carlosevelo96@gmail.com>
2023-02-16 11:31:57 -0700

    color and advice

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,16 +6,35 @@
 //  Copyright © 2023 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
-    var bmi: Float?
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more!", color: .blue)
+        }
+        else if bmiValue > 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Eat less!", color: .red)
+        }
+        else {
+            bmi = BMI(value: bmiValue, advice: "Eat the same!", color: .green)
+        }
+        
     }
     
     func getBMIValue() -> String {
-        return String(format: "%.1f", bmi ?? "")
+        return String(format: "%.1f", bmi?.value ?? 0.0)
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No Advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .gray
     }
 }
